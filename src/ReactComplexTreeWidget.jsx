f01ed02a9bb6c83e5f0e543b8181649df5c6e43f
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2023-10-09 18:14:30 +0200

    Move CSS to Mendix module

--- src/ReactComplexTreeWidget.jsx
+++ src/ReactComplexTreeWidget.jsx
@@ -2,7 +2,6 @@ import { TreeContainer } from "./components/TreeContainer";
 import { createElement } from "react";
 
 // eslint-disable-next-line sort-imports
-import "./ui/ReactComplexTreeWidget.css";
 import "react-complex-tree/lib/style-modern.css";
 
 export function ReactComplexTreeWidget(props) {
